0e05e371a5ff2ebd90117910c8cc4a36f8764095
Umayr Shaffi <ushaffi95@gmail.com>
2019-10-15 11:53:44 +0100

    completed util functins with TDD and seeded db sucessfully, now building endpoints using TDD

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -24,4 +24,19 @@ exports.makeRefObj = (list) => {
 	return refObj;
 };
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {
+	if (!comments) return [];
+	const formatted = [];
+
+	comments.forEach((comment) => {
+		let newObj = { ...comment };
+		newObj.author = newObj.created_by;
+		delete newObj.created_by;
+		newObj.article_id = articleRef[newObj.belongs_to];
+		delete newObj.belongs_to;
+		newObj.created_at = new Date(newObj.created_at);
+		formatted.push(newObj);
+	});
+
+	return formatted;
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -9,10 +9,20 @@ describe('formatDates', () => {
 		expect(formatDates([])).to.eql([]);
 	});
 	it('can convert a timestamp into a date object', () => {
-		expect(formatDates([ { created_at: 975242163389 } ])).to.eql([ { created_at: new Date(975242163389) } ]);
+		expect(
+			formatDates([
+				{ created_at: 975242163389 }
+			])
+		).to.eql([
+			{ created_at: new Date(975242163389) }
+		]);
 	});
 	it('return an array of multiple objects with the appropriate key and date', () => {
-		const objsArr = [ { created_at: 975242163389 }, { created_at: 1006778163389 }, { created_at: 1038314163389 } ];
+		const objsArr = [
+			{ created_at: 975242163389 },
+			{ created_at: 1006778163389 },
+			{ created_at: 1038314163389 }
+		];
 
 		const output = [
 			{ created_at: new Date(975242163389) },
@@ -23,7 +33,11 @@ describe('formatDates', () => {
 		expect(formatDates(objsArr)).to.eql(output);
 	});
 	it('doesnt mutate the original array', () => {
-		const objsArr = [ { created_at: 975242163389 }, { created_at: 1006778163389 }, { created_at: 1038314163389 } ];
+		const objsArr = [
+			{ created_at: 975242163389 },
+			{ created_at: 1006778163389 },
+			{ created_at: 1038314163389 }
+		];
 		const unAltered = [
 			{ created_at: 975242163389 },
 			{ created_at: 1006778163389 },
@@ -96,4 +110,130 @@ describe('makeRefObj', () => {
 	});
 });
 
-describe('formatComments', () => {});
+describe.only('formatComments', () => {
+	it('returns an array', () => {
+		expect(formatComments()).to.be.an('array');
+	});
+	it('returns an empty array if passed through an empty array', () => {
+		expect(formatComments()).to.eql([]);
+	});
+	it('can format a comment object', () => {
+		const comment = [
+			{
+				body:
+					'What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.',
+				belongs_to: 'UNCOVERED: catspiracy to bring down democracy',
+				created_by: 'icellusedkars',
+				votes: 16,
+				created_at: 1101386163389
+			}
+		];
+
+		const refObj = { 'UNCOVERED: catspiracy to bring down democracy': 1 };
+
+		const output = [
+			{
+				body:
+					'What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.',
+				article_id: 1,
+				author: 'icellusedkars',
+				votes: 16,
+				created_at: new Date(1101386163389)
+			}
+		];
+		expect(formatComments(comment, refObj)).to.eql(output);
+	});
+	it('can format an array of comment objects', () => {
+		const comment = [
+			{
+				body:
+					'What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.',
+				belongs_to: 'UNCOVERED: catspiracy to bring down democracy',
+				created_by: 'icellusedkars',
+				votes: 16,
+				created_at: 1101386163389
+			},
+			{
+				body:
+					'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+				belongs_to: 'Living in the shadow of a great man',
+				created_by: 'butter_bridge',
+				votes: 14,
+				created_at: 1479818163389
+			}
+		];
+
+		const refObj = {
+			'UNCOVERED: catspiracy to bring down democracy': 1,
+			'Living in the shadow of a great man': 5
+		};
+
+		const output = [
+			{
+				body:
+					'What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.',
+				article_id: 1,
+				author: 'icellusedkars',
+				votes: 16,
+				created_at: new Date(1101386163389)
+			},
+			{
+				body:
+					'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+				article_id: 5,
+				author: 'butter_bridge',
+				votes: 14,
+				created_at: new Date(1479818163389)
+			}
+		];
+		expect(formatComments(comment, refObj)).to.eql(output);
+	});
+	it('doesnt mutate original data', () => {
+		const comment = [
+			{
+				body:
+					'What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.',
+				belongs_to: 'UNCOVERED: catspiracy to bring down democracy',
+				created_by: 'icellusedkars',
+				votes: 16,
+				created_at: 1101386163389
+			},
+			{
+				body:
+					'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+				belongs_to: 'Living in the shadow of a great man',
+				created_by: 'butter_bridge',
+				votes: 14,
+				created_at: 1479818163389
+			}
+		];
+
+		const unaltered = [
+			{
+				body:
+					'What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.',
+				belongs_to: 'UNCOVERED: catspiracy to bring down democracy',
+				created_by: 'icellusedkars',
+				votes: 16,
+				created_at: 1101386163389
+			},
+			{
+				body:
+					'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+				belongs_to: 'Living in the shadow of a great man',
+				created_by: 'butter_bridge',
+				votes: 14,
+				created_at: 1479818163389
+			}
+		];
+		const refObj = {
+			'UNCOVERED: catspiracy to bring down democracy': 1,
+			'Living in the shadow of a great man': 5
+		};
+		const formatted = formatComments(comment, refObj);
+
+		expect(comment).to.not.equal(formatted);
+		expect(comment[0]).to.not.eql(formatted[0]);
+		expect(comment[0]).to.eql(unaltered[0]);
+	});
+});
